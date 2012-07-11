40639dd6c0bbd9eaffaf5c69373b4c61f55d54cc
Macil Tech <maciltech@gmail.com>
2012-07-11 17:34:11 -0600

    Grabbed Alyssa Rowan's fixes to inline-expanding.js.

--- js/inline-expanding.js
+++ js/inline-expanding.js
@@ -3,45 +3,44 @@
  * https://github.com/savetheinternet/Tinyboard/blob/master/js/inline-expanding.js
  *
  * Released under the MIT license
- * Copyright (c) 2012 Michael Save <savetheinternet@tinyboard.org>
+ * Copyright (c) 2012 Michael Save <savetheinternet@tinyboard.org> &
+ * Alyssa Rowan <alyssa.rowan@gmail.com>
  *
  * Usage:
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
  *   $config['additional_javascript'][] = 'js/inline-expanding.js';
  *
  */
-
-onready(function(){
-	var link = document.getElementsByTagName('a');
-	
-	for(var i = 0; i < link.length; i++) {
-		if(typeof link[i] == "object" && link[i].childNodes[0].src && link[i].className != 'file') {
-			link[i].childNodes[0].style.maxWidth = '95%';
-			link[i].childNodes[0].style.maxHeight = '95%';
-			link[i].onclick = function(e) {
-				if(e.which == 2) {
-					return true;
-				}
-				if(!this.tag) {
-					this.tag = this.childNodes[0].src;
-					this.childNodes[0].src = this.href;
-					this.childNodes[0].style.width = 'auto';
-					this.childNodes[0].style.height = 'auto';
-					this.childNodes[0].style.opacity = '0.4';
-					this.childNodes[0].style.filter = 'alpha(opacity=40)';
-					this.childNodes[0].onload = function() {
-						this.style.opacity = '1';
-						this.style.filter = '';
-					}
-				} else {
-					this.childNodes[0].src = this.tag;
-					this.childNodes[0].style.width = 'auto';
-					this.childNodes[0].style.height = 'auto';
-					this.tag = '';
-				}
-				return false;
+$(document).ready(function(){
+	var init_expand_image = function() {
+		$(this).parent().click(function(e) {
+			if(e.which == 2) {
+				return true;
 			}
-			
-		}
-	}
+			if(!this.tag) {
+				this.tag = this.childNodes[0].src;
+				this.childNodes[0].src = this.href;
+				this.childNodes[0].style.width = 'auto';
+				this.childNodes[0].style.maxWidth = '100%';
+				this.childNodes[0].style.height = 'auto';
+				this.childNodes[0].style.opacity = '0.4';
+				this.childNodes[0].style.filter = 'alpha(opacity=40)';
+				this.childNodes[0].onload = function() {
+					this.style.opacity = '1';
+					this.style.filter = '';
+				}
+			} else {
+				this.childNodes[0].src = this.tag;
+				this.childNodes[0].style.width = 'auto';
+				this.childNodes[0].style.height = 'auto';
+				this.tag = '';
+			}
+			return false;
+		});
+	};
+	$('div.threadblock>a:not([class="file"])>img').each(init_expand_image);
+	$('div.post>a:not([class="file"])>img').each(init_expand_image);
+	$(document).bind('new_post', function(e, post) {
+		$(post).find('>a:not([class="file"])>img').each(init_expand_image);
+	});
 });
-
