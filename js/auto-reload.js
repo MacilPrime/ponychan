4ceef14df2430196ac8d8297579d939a72dc90c4
Macil Tech <maciltech@gmail.com>
2012-07-11 17:34:20 -0600

    auto-reload.js now works more like 4chan X. It doesn't only refresh when at the bottom of the page.

--- js/auto-reload.js
+++ js/auto-reload.js
@@ -16,36 +16,87 @@
 $(document).ready(function(){
 	if($('div.banner').length == 0)
 		return; // not index
+
+	var $statusBox = $("<div/>")
+		.attr("id", "updateStatus")
+		.css("position", "fixed")
+		.css("bottom", 0)
+		.css("right", 0)
+		.appendTo(document.body);
+
+	var $postsAdded = $("<span/>").text("+0");
+	var $countDown = $("<span/>");
+	$statusBox.append($postsAdded, " ", $countDown);
 	
-	var poll_interval;
+	var updateInterval = 30;
+	var retryInterval = 10;
+
+	var tickTimer = null;
+	var query = null;
+	var timeUntilUpdate = updateInterval+1;
+
+	var tick = function() {
+		timeUntilUpdate--;
+		if(timeUntilUpdate > 0) {
+			$countDown.text("-"+timeUntilUpdate);
+			tickTimer = setTimeout(tick, 1000);
+		} else {
+			downloadNewPosts();
+		}
+	};
 	
-	var poll = function() {
-		$.ajax({
+	var downloadNewPosts = function() {
+		if(tickTimer)
+			clearTimeout(tickTimer);
+
+		$countDown.text("-0");
+
+		if(query)
+			query.abort();
+
+		query = $.ajax({
 			url: document.location,
 			success: function(data) {
+				var postsAddedCount = 0;
 				$(data).find('div.post.reply').each(function() {
 					var id = $(this).attr('id');
 					if($('#' + id).length == 0) {
 						$(this).insertAfter($('div.post:last').next()).after('<br class="clear">');
 						$(document).trigger('new_post', this);
+						postsAddedCount++;
 					}
 				});
+				$postsAdded.text("+"+postsAddedCount);
+				timeUntilUpdate = updateInterval+1;
+				tick();
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				if(textStatus == "abort")
+					return;
+				if(textStatus == "error" && errorThrown == "Not Found") {
+					$statusBox.css('color', 'red').text('404');
+					return;
+				}
+				$postsAdded.text("Retrying");
+				timeUntilUpdate = retryInterval+1;
+				tick();
+			},
+			complete: function() {
+				query = null;
 			}
 		});
-		
-		poll_interval = setTimeout(poll, 5000);
 	};
-	
-	$(window).scroll(function() {
-		if($(this).scrollTop() + $(this).height() < $('div.post:last').position().top + $('div.post:last').height()) {
-			clearTimeout(poll_interval);
-			poll_interval = false;
+
+	$(document).keydown(function(event) {
+		if(/TEXTAREA|INPUT/.test(event.target.nodeName))
 			return;
+
+		if(event.which == 85) {
+			if(timeUntilUpdate < updateInterval)
+				downloadNewPosts();
 		}
-		
-		if(poll_interval === false) {
-			poll_interval = setTimeout(poll, 1500);
-		}
-	}).trigger('scroll');
+	});
+
+	tick();
 });
 
