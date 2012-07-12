7b3bf610e27bc644761e3e73daad23e05f36b162
Macil Tech <maciltech@gmail.com>
2012-07-12 10:04:37 -0600

    Added tags.js which adds combos for [b], [i], and [spoiler] tags.

---
+++ js/tags.js
@@ -0,0 +1,37 @@
+/*
+ * qr.js
+ *
+ * Usage:
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
+ *   $config['additional_javascript'][] = 'js/tags.js';
+ *
+ */
+
+$(document).ready(function(){
+	$(document).keydown(function(event) {
+		if(!/TEXTAREA/.test(event.target.nodeName))
+			return true;
+
+		if(!event.ctrlKey)
+			return true;
+
+		var tag;
+		switch(event.which) {
+		case 66: tag = 'b'; break;
+		case 73: tag = 'i'; break;
+		case 83: tag = 'spoiler'; break;
+		default:
+			return true;
+		}
+
+		if(typeof event.target.selectionStart == "undefined" || event.target.selectionStart == null)
+			return true;
+
+		var text = $(event.target).val();
+		var start = event.target.selectionStart;
+		var end = event.target.selectionEnd;
+		text = text.slice(0,start) + '['+tag+']' + text.slice(start,end) + '[/'+tag+']' + text.slice(end);
+		$(event.target).val(text);
+		return false;
+	});
+});
