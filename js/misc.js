6f8996e34acbb3350420962cc73af5deb2458c31
Macil Tech <maciltech@gmail.com>
2012-07-13 14:51:29 -0600

    Shorten secret entrance timer, and change href to support middle clicking.

--- js/misc.js
+++ js/misc.js
@@ -9,22 +9,17 @@
 
 $(document).ready(function(){
 	var $secondLine = $("footer .unimportant").slice(1,2);
+	var $targetLink = $("footer .unimportant a").slice(1,2);
 
-	var prepped = false;
+	var oldLink = $targetLink.attr("href");
+	var newLink = "/mod.php";
+
 	var unprepTimer = null;
 	$secondLine.dblclick(function() {
-		prepped = true;
 		clearTimeout(unprepTimer);
+		$targetLink.attr("href", newLink);
 		unprepTimer = setTimeout(function() {
-			prepped = false;
-		}, 15*1000);
-	});
-
-	var $secondLink = $("footer .unimportant a").slice(1,2);
-	$secondLink.click(function() {
-		if(prepped) {
-			window.location.href = "/mod.php";
-			return false;
-		}
+			$targetLink.attr("href", oldLink);
+		}, 5*1000);
 	});
 });
