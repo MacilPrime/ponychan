d76fcd2e8f60f7def84eccca11db7f1a90f19b4f
Macil Tech <maciltech@gmail.com>
2012-08-16 09:26:27 -0500

    Don't close <img> tags either.

--- inc/display.php
+++ inc/display.php
@@ -195,7 +195,7 @@ function truncate($body, $url, $max_lines = false, $max_chars = false) {
 			// remove broken HTML entity at the end (if existent)
 			$body = preg_replace('/&[^;]+$/', '', $body);
 			
-			$tags_no_close_needed = array("colgroup", "dd", "dt", "li", "optgroup", "option", "p", "tbody", "td", "tfoot", "th", "thead", "tr", "br");
+			$tags_no_close_needed = array("colgroup", "dd", "dt", "li", "optgroup", "option", "p", "tbody", "td", "tfoot", "th", "thead", "tr", "br", "img");
 			
 			// Close any open tags
 			foreach ($tags as &$tag) {
