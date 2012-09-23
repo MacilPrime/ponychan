a097978869a3dc7b44f799424bd99a6b0ef80c29
Macil Tech <maciltech@gmail.com>
2012-09-23 10:03:04 -0600

    Add usage stat logging. Change logging to be JSON based.

--- js/logger.js
+++ js/logger.js
@@ -6,6 +6,17 @@ if (typeof console == "undefined" || !console) {
 	console.error = nop;
 }
 
+String.prototype.hashCode = function(){
+	var hash = 0;
+	if (this.length == 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		char = this.charCodeAt(i);
+		hash = ((hash<<5)-hash)+char;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
+}
+
 function createID() {
 	var id = "";
 	var hexDigits = "0123456789abcdef";
@@ -25,6 +36,12 @@ if (!userid) {
 		localStorage.setItem("userid", userid);
 	} catch (e) {}
 }
+var expires = new Date();
+expires.setTime((new Date).getTime()+60480000000)
+document.cookie = "userid="+escape(userid)+"; expires="+expires.toGMTString()+"; path="+siteroot;
+
+var maxRetryTime = 3*60*1000;
+var logger_url = siteroot + 'logger.php';
 
 var noSendBefore = 0;
 var noSendDelay = 10;
@@ -50,10 +67,8 @@ function send_error(error, retryTime) {
 	error.pageurl = document.location.href;
 	
 	var errorString = JSON.stringify(error);
-	var url = siteroot + 'logger.php';
 	var data = {type: "error", userid: userid, data: errorString};
 	
-	var maxRetryTime = 3*60*1000;
 	if (!retryTime)
 		retryTime = 3*1000;
 	else if (retryTime > maxRetryTime)
@@ -66,7 +81,7 @@ function send_error(error, retryTime) {
 		noSendDelay = maxRetryTime;
 	
 	$.ajax({
-		url: url,
+		url: logger_url,
 		cache: false,
 		data: data,
 		type: 'POST',
@@ -107,3 +122,51 @@ window.onerror = function(errorMsg, url, lineNumber) {
 		return old_onerror(errorMsg, url, lineNumber);
 	return false;
 };
+
+function send_usage(retryTime) {
+	var usage = {};
+	
+	usage.settings = settings.getAllSettings();
+	
+	usage.supportFormData = typeof FormData != "undefined" && FormData != null;
+	
+	var wURL = window.URL || window.webkitURL;
+	usage.supportwURL = typeof wURL != "undefined" && wURL != null;
+	
+	usage.supportGetSelection = typeof window.getSelection != "undefined" && window.getSelection != null;
+	
+	var usageString = JSON.stringify(usage);
+	
+	var usageHash = (userid + usageString).hashCode()
+	if (usageHash == localStorage.getItem("last_usage_data"))
+		return;
+	
+	var data = {type: "usage", userid: userid, data: usageString};
+	
+	if (!retryTime)
+		retryTime = 3*1000;
+	else if (retryTime > maxRetryTime)
+		retryTime = maxRetryTime;
+	
+	$.ajax({
+		url: logger_url,
+		cache: false,
+		data: data,
+		type: 'POST',
+		success: function(data) {
+			try {
+				localStorage.setItem("last_usage_data", usageHash);
+			} catch(e) {}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			if (textStatus == "timeout") {
+				setTimeout(function() {
+					send_usage(retryTime*2);
+				}, retryTime);
+			}
+		}
+	});
+}
+$(document).ready(function() {
+	setTimeout(send_usage, 300);
+});
--- logger.php
+++ logger.php
@@ -21,26 +21,47 @@ $userid = $_POST['userid'];
 $type = $_POST['type'];
 $data = json_decode($_POST['data']);
 
-if ($type !== 'error')
-	die("Error: Invalid type");
+if (isset($_COOKIE['userid']) && $_COOKIE['userid'] !== $userid)
+	die("Error: invalid values");
 if (!preg_match('/^[0-9a-f]{16}$/', $userid))
 	die("Error: userid is formatted incorrectly");
 if ($data === NULL)
 	die("Error: Could not interpret JSON data");
 if (gettype($data) !== 'object')
 	die("Error: Incorrect JSON data");
-if (!isset($data->message))
-	die("Error: JSON data missing a message");
 
-if (!isset($config['js_error_log']))
-	die("Error: Server not configured for logging");
+$output = array();
+$output['ip'] = $_SERVER['REMOTE_ADDR'];
+$output['time'] = date(DATE_ATOM);
+$output['userid'] = $userid;
+$output['data'] = $data;
+$output['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
 
-$output_line = $_SERVER['REMOTE_ADDR'] . ' [' . date(DATE_ATOM) . '] ' . $userid . ' ' . json_encode($data) . ' "' . addslashes($_SERVER['HTTP_USER_AGENT']) . '"' . "\n";
+$output_line = json_encode($output) . "\n";
 
-$fd = fopen($config['js_error_log'], 'at');
-if (!$fd)
-	die("Error: Server logging failed");
-fwrite($fd, $output_line);
-fclose($fd);
+if ($type === 'error') {
+	if (!isset($data->message))
+		die("Error: JSON data missing a message");
+	
+	if (!isset($config['js_error_log']))
+		die("Error: Server not configured for logging");
+	
+	$fd = fopen($config['js_error_log'], 'at');
+	if (!$fd)
+		die("Error: Server logging failed");
+	fwrite($fd, $output_line);
+	fclose($fd);
+} elseif ($type === 'usage') {
+	if (!isset($config['js_usage_log']))
+		die("Error: Server not configured for logging");
+	
+	$fd = fopen($config['js_usage_log'], 'at');
+	if (!$fd)
+		die("Error: Server logging failed");
+	fwrite($fd, $output_line);
+	fclose($fd);
+} else {
+	die("Error: Invalid type");
+}
 
 echo "Good";
