66f262468d6bd818e98ba6de5ff46dcd9884c030
Macil Tech <maciltech@gmail.com>
2013-02-22 18:51:51 +0800

    Show IP on hover when obscure IPs setting is set.

--- js/ips.js
+++ js/ips.js
@@ -3,14 +3,31 @@ settings.newProp("mod_obscure_ips", "bool", false, "Obscure user IP addresses");
 $(document).ready(function() {
 	var obscure_ips = settings.getProp("mod_obscure_ips");
 	
+	function getIPfromlink($a) {
+		var m = /^\?\/IP\/(.*)$/.exec($a.attr('href'));
+		return m[1];
+	}
+
 	var processIPs = function(context) {
 		if (obscure_ips) {
-			$(".posterip a", context).text("IP").addClass('ip-hidden');
+			$(".posterip a", context)
+				.text("IP")
+				.addClass('ip-hidden')
+				.on('mouseenter.iphider', function() {
+					var $this = $(this);
+					$this.text(getIPfromlink($this));
+				})
+				.on('mouseleave.iphider', function() {
+					var $this = $(this);
+					$this.text("IP");
+				});
 		} else {
 			$("a.ip-hidden", context).each(function() {
 				var $this = $(this);
-				var m = /^\?\/IP\/(.*)$/.exec($this.attr('href'));
-				$this.text(m[1]);
+				$this
+					.text(getIPfromlink($this))
+					.removeClass('ip-hidden')
+					.off('.iphider');
 			});
 		}
 	}
