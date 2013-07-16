e93a718f1d0263ae741cac37f74032bfb1497577
Macil Tech <maciltech@gmail.com>
2013-07-16 05:51:32 -0500

    Correct the location of whatever immediately follows the navbar.

--- js/navbar.js
+++ js/navbar.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 		} else {
 			$navbar.css("position", "absolute");
 		}
-		$("header, .top_notice").css("margin-top", (16+$navbar.height())+"px");
+		$(".boardlist.top + *").css("margin-top", (16+$navbar.height())+"px");
 	};
 
 	navbarApplySettings();
