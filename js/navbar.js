25354e53af5f9443d511d6a811b1a8548b049d8e
Macil Tech <maciltech@gmail.com>
2013-10-16 13:06:47 -0500

    When the navbar is fixed position, make sure if the page hash is set to a post and we scroll to it, the post isn't covered by the navbar at all.

--- js/navbar.js
+++ js/navbar.js
@@ -19,12 +19,22 @@
 $(document).ready(function(){
 	var navbarApplySettings = function() {
 		var $navbar = $(".boardlist.top");
+		var $nav_style = $("style#nav_style");
+		
 		if (settings.getSetting("fixed_navbar")) {
 			$navbar.css("position", "fixed");
+			if (!$nav_style.length) {
+				$nav_style = $("<style/>")
+					.attr("id", "nav_style")
+					.attr("type", "text/css")
+					.appendTo(document.head);
+			}
+			$nav_style.text(".jumpHandle { position: relative; top: "+(-16-$navbar.outerHeight())+"px; }");
 		} else {
 			$navbar.css("position", "absolute");
+			$nav_style.text("");
 		}
-		$(".boardlist.top + *").css("margin-top", (16+$navbar.height())+"px");
+		$(".boardlist.top + *").css("margin-top", (16+$navbar.outerHeight())+"px");
 	};
 
 	navbarApplySettings();
