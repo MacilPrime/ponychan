9129ebd696b4eeff27389e4904cdf0f949dc4530
Macil Tech <maciltech@gmail.com>
2014-09-30 23:02:57 -0700

    Break up long line so Atom doesn't fail at syntax highlighting the file.

--- SERVER/inc/functions.php
+++ SERVER/inc/functions.php
@@ -908,7 +908,16 @@ function threadExists($id) {
 
 function post(array $post) {
 	global $pdo, $board;
-	$query = prepare(sprintf("INSERT INTO `posts_%s` (`id`, `thread`, `subject`, `email`, `name`, `trip`, `capcode`, `body`, `body_nomarkup`, `time`, `bump`, `thumb`, `thumbwidth`, `thumbheight`, `file`, `filewidth`, `fileheight`, `filesize`, `filename`, `filehash`, `password`, `ip`, `sticky`, `locked`, `sage`, `embed`, `mature`) VALUES ( NULL, :thread, :subject, :email, :name, :trip, :capcode, :body, :body_nomarkup, :time, :time, :thumb, :thumbwidth, :thumbheight, :file, :width, :height, :filesize, :filename, :filehash, :password, :ip, :sticky, :locked, 0, :embed, :mature)", $board['uri']));
+	$query = prepare(sprintf(
+		'INSERT INTO `posts_%s` (`id`, `thread`, `subject`, `email`, `name`, ' .
+		'`trip`, `capcode`, `body`, `body_nomarkup`, `time`, `bump`, `thumb`, ' .
+		'`thumbwidth`, `thumbheight`, `file`, `filewidth`, `fileheight`, ' .
+		'`filesize`, `filename`, `filehash`, `password`, `ip`, `sticky`, ' .
+		'`locked`, `sage`, `embed`, `mature`) VALUES ( NULL, :thread, :subject, ' .
+		':email, :name, :trip, :capcode, :body, :body_nomarkup, :time, :time, ' .
+		':thumb, :thumbwidth, :thumbheight, :file, :width, :height, :filesize, ' .
+		':filename, :filehash, :password, :ip, :sticky, :locked, 0, :embed, ' .
+		':mature)', $board['uri']));
 
 	// Basic stuff
 	if (!empty($post['subject'])) {
