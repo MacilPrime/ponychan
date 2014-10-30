953799b3098c7211e0cdcc17bc5e62a4fcef9571
Macil Tech <maciltech@gmail.com>
2014-10-30 02:10:25 -0700

    Automatic rebuilds.

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,76 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var mold = require('mold-source-map');
 var streamify = require('gulp-streamify');
 var sourcemaps = require('gulp-sourcemaps');
-var util = require('util');
 var stdio = require('stdio');
+var gutil = require('gulp-util');
 
 var args = stdio.getopt({
-  // 'watch': {key: 'w', description: 'Automatic rebuild'},
+  'watch': {key: 'w', description: 'Automatic rebuild'},
   // 'minify': {key: 'm', description: 'Minify build'}
 });
+
+function copyTask(name, paths) {
+  gulp.task(name, function() {
+    return gulp.src(paths)
+      .pipe(gulp.dest('SERVER/js/'));
+  });
+  if (args.watch) {
+    gulp.watch(paths, [name]);
+  }
+}
+
+function browserifyTask(name, entry, destname) {
+  gulp.task(name, function() {
+    var bundler = browserify({
+      debug: true,
+      entries: entry,
+      noparse: ['jquery', 'moment', 'baconjs', 'rsvp', 'underscore'],
+      cache: {}, packageCache: {}, fullPaths: args.watch
+    });
+
+    if (args.watch) {
+      bundler = watchify(bundler);
+      bundler.on('update', buildBundle.bind(null, true));
+    }
+
+    function buildBundle(isRebuild) {
+      var bundle = bundler.bundle();
+      var result = bundle
+        .pipe(mold.transformSourcesRelativeTo('.'))
+        .pipe(source(destname))
+        .pipe(streamify(sourcemaps.init({loadMaps:true})))
+        .pipe(streamify(sourcemaps.write('.')))
+        .pipe(gulp.dest('SERVER/js/'));
+
+      if (isRebuild) {
+        var wasError = false;
+        gutil.log("Rebuilding '"+gutil.colors.cyan(name)+"'");
+        bundle.on('error', function(err) {
+          wasError = true;
+          gutil.log(gutil.colors.red("Error")+" rebuilding '"+gutil.colors.cyan(name)+"':"+err.message);
+          result.end();
+        });
+        result.on('end', function() {
+          if (!wasError) {
+            gutil.log("Finished rebuild of '"+gutil.colors.cyan(name)+"'");
+          }
+        });
+      }
+
+      return result;
+    }
+
+    return buildBundle(false);
+  });
+}
 
 gulp.task('default', ['build']);
 
 gulp.task('build', ['client-js-extra', 'main-js']);
 
-gulp.task('client-js-extra', function() {
-  return gulp.src('jsextra/**.js')
-    .pipe(gulp.dest('SERVER/js/'));
-});
-
-gulp.task('main-js', function() {
-  var bundler = browserify({
-    debug: true,
-    entries: ['./src/main.js/'],
-    noparse: ['jquery', 'moment', 'baconjs', 'rsvp', 'underscore']
-  });
-
-  var bundle = bundler.bundle();
-  var result = bundle
-    .pipe(mold.transformSourcesRelativeTo('.'))
-    .pipe(source('main.js'))
-    .pipe(streamify(sourcemaps.init({loadMaps:true})))
-    .pipe(streamify(sourcemaps.write('.')))
-    .pipe(gulp.dest('SERVER/js/'));
-
-  return result;
-});
+copyTask('client-js-extra', './src/extra/**.js');
+browserifyTask('main-js', './src/main.js/', 'main.js');
