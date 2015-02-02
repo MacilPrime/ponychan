e0ef6c2e638da669e3c3fdc472043b2a83e39eaa
Macil Tech <maciltech@gmail.com>
2015-02-02 00:32:18 -0800

    Set reasonable defaults for mlpchan theme.

--- core/templates/themes/mlpchan/info.php
+++ core/templates/themes/mlpchan/info.php
@@ -1,15 +1,15 @@
 <?php
 	$theme = Array();
-	
+
 	// Theme name
 	$theme['name'] = 'MLPchan';
 	// Description (you can use Tinyboard markup here)
 	$theme['description'] = 'MLPchan stuff. Enabling boardlinks is recommended for this theme.';
 	$theme['version'] = 'v0.1';
-	
-	// Theme configuration	
+
+	// Theme configuration
 	$theme['config'] = Array();
-		
+
 	$theme['config'][] = Array(
 		'title' => 'Slogan',
 		'name' => 'subtitle',
@@ -17,7 +17,7 @@
 		'default' => 'Friendship is Magic!',
 		'comment' => '(optional)'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => 'File',
 		'name' => 'file',
@@ -25,31 +25,31 @@
 		'default' => $config['file_index'],
 		'comment' => '(eg. "index.html")'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => '# of news recent entries',
 		'name' => 'no_recent',
 		'type' => 'text',
-		'default' => 0,
+		'default' => 3,
 		'size' => 3,
 		'comment' => '(number of recent news entries to display; "-1" is infinite)'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => 'Excluded boards',
 		'name' => 'exclude',
 		'type' => 'text',
 		'comment' => '(space seperated)'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => '# of recent posts',
 		'name' => 'limit_posts',
 		'type' => 'text',
-		'default' => '4',
+		'default' => '8',
 		'comment' => '(maximum posts to display)'
 	);
-	
+
 	// Unique function name for building everything
 	$theme['build_function'] = 'mlpchan_build';
 	$theme['install_callback'] = 'mlpchan_install';
