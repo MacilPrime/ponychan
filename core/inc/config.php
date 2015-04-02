2d01f534b3defa7709df8aeaac99977fe18429ec
Macil Tech <maciltech@gmail.com>
2015-04-02 03:03:06 -0700

    Get Octavia ready

--- core/inc/config.php
+++ core/inc/config.php
@@ -717,7 +717,7 @@
 	$config['stylesheets']['Nightmare Moon'] = array('Nightmare Moon', 'nightmare-moon.css');
 	// $config['stylesheets']['Vinyl Trance'] = array('Vinyl Trance', 'vinyl.css');
 	// $config['stylesheets']['Timelord'] = array('Timelord', 'timelord.css');
-	// $config['stylesheets']['Octavia'] = array('Octavia', 'octavia.css');
+	$config['stylesheets']['Octavia'] = array('Octavia', 'octavia.css');
 	// $config['stylesheets']['Great and Powerful'] = array('Great and Powerful', 'trixie.css');
 	// $config['stylesheets']['Aloe and Lotus'] = array('Aloe and Lotus', 'aloe-and-lotus.css');
 	// $config['stylesheets']['Scootaloo'] = array('Scootaloo', 'scootaloo.css');
