3aee4deb0f4593dbd9575e5828176a5311fc859d
Macil Tech <maciltech@gmail.com>
2015-04-02 01:21:38 -0700

    get nightmare moon theme ready

--- core/inc/config.php
+++ core/inc/config.php
@@ -714,7 +714,7 @@
 	// $config['stylesheets']['Fluttershy'] = array('Fluttershy', 'fluttershy.css');
 	// $config['stylesheets']['Applejack'] = array('Applejack', 'applejack.css');
 	// $config['stylesheets']['Rarity'] = array('Rarity', 'rarity.css');
-	// $config['stylesheets']['Nightmare Moon'] = array('Nightmare Moon', 'nightmare-moon.css');
+	$config['stylesheets']['Nightmare Moon'] = array('Nightmare Moon', 'nightmare-moon.css');
 	// $config['stylesheets']['Vinyl Trance'] = array('Vinyl Trance', 'vinyl.css');
 	// $config['stylesheets']['Timelord'] = array('Timelord', 'timelord.css');
 	// $config['stylesheets']['Octavia'] = array('Octavia', 'octavia.css');
