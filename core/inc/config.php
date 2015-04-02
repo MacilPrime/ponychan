4987275d8b8049e01b787f64c3827b4f58093d93
Macil Tech <maciltech@gmail.com>
2015-04-02 03:06:19 -0700

    enable fluttershy

--- core/inc/config.php
+++ core/inc/config.php
@@ -711,7 +711,7 @@
 	$config['stylesheets']['Twilight'] = array('Twilight', 'twilight.css');
 	$config['stylesheets']['Twilight Tackle'] = array('Twilight Tackle', 'twilight-tackle.css');
 	$config['stylesheets']['Rainbow Dash'] = array('Rainbow Dash', 'rainbow-dash.css');
-	// $config['stylesheets']['Fluttershy'] = array('Fluttershy', 'fluttershy.css');
+	$config['stylesheets']['Fluttershy'] = array('Fluttershy', 'fluttershy.css');
 	// $config['stylesheets']['Applejack'] = array('Applejack', 'applejack.css');
 	// $config['stylesheets']['Rarity'] = array('Rarity', 'rarity.css');
 	$config['stylesheets']['Nightmare Moon'] = array('Nightmare Moon', 'nightmare-moon.css');
