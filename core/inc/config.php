ba41360e9add8aebcadf4cc5c5cbee655a4158c9
Macil Tech <maciltech@gmail.com>
2015-04-02 02:49:04 -0700

    get rainbow dash theme ready

--- core/inc/config.php
+++ core/inc/config.php
@@ -710,7 +710,7 @@
 
 	$config['stylesheets']['Twilight'] = array('Twilight', 'twilight.css');
 	$config['stylesheets']['Twilight Tackle'] = array('Twilight Tackle', 'twilight-tackle.css');
-	// $config['stylesheets']['Rainbow Dash'] = array('Rainbow Dash', 'rainbow-dash.css');
+	$config['stylesheets']['Rainbow Dash'] = array('Rainbow Dash', 'rainbow-dash.css');
 	// $config['stylesheets']['Fluttershy'] = array('Fluttershy', 'fluttershy.css');
 	// $config['stylesheets']['Applejack'] = array('Applejack', 'applejack.css');
 	// $config['stylesheets']['Rarity'] = array('Rarity', 'rarity.css');
