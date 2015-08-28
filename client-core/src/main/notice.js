7b4effd5e27e278d7579181933a813c6e408f830
Macil Tech <maciltech@gmail.com>
2015-08-27 23:04:27 -0700

    bacon->kefir merge fix

--- client-core/src/main/notice.js
+++ client-core/src/main/notice.js
@@ -8,7 +8,7 @@
 
 import $ from 'jquery';
 import RSVP from 'rsvp';
-import Bacon from 'baconjs';
+import Kefir from 'kefir';
 
 export function settingsAd(text, time) {
 	return pop(text+' \u2191', time);
@@ -32,13 +32,13 @@ export function pop(text, time = 30) {
 		.appendTo(document.body);
 
 	return new RSVP.Promise(resolve => {
+		// convert seconds to milliseconds.
 		const seconds = s => s * 1000;
-		// Above function is to convert seconds to milliseconds.
 
-		Bacon.mergeAll(
-			Bacon.fromEvent($pop, 'click'),
-			Bacon.later(seconds(time))
-		).take(1)
+		Kefir.merge([
+			Kefir.fromEvents($pop, 'click'),
+			Kefir.later(seconds(time))
+		]).take(1)
 			.onValue(() => $pop.fadeOut(seconds(1), resolve));
 	}).then(() => {
 			$pop.nextAll('.popnotice')
