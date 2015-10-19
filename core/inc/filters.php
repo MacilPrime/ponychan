6c12e50104562f611125b4ea7f2df340d5448c9c
Macil Tech <maciltech@gmail.com>
2015-10-19 00:13:21 -0700

    remove unused reject property from filters

--- core/inc/filters.php
+++ core/inc/filters.php
@@ -11,15 +11,15 @@ if (realpath($_SERVER['SCRIPT_FILENAME']) == str_replace('\\', '/', __FILE__)) {
 
 class Filter {
 	private $condition;
-	
+
 	public function __construct(array $arr) {
 		foreach ($arr as $key => $value)
-			$this->$key = $value;		
+			$this->$key = $value;
 	}
-	
+
 	public function match(array $post, $condition, $match) {
 		$condition = strtolower($condition);
-		
+
 		switch($condition) {
 			case 'custom':
 				if (!is_callable($match))
@@ -53,41 +53,36 @@ class Filter {
 				error('Unknown filter condition: ' . $condition);
 		}
 	}
-	
+
 	public function action() {
 		global $board;
-		
+
 		switch($this->action) {
 			case 'reject':
 				error(isset($this->message) ? $this->message : 'Posting throttled by flood filter.');
 			case 'ban':
 				if (!isset($this->reason))
 					error('The ban action requires a reason.');
-				
+
 				$reason = $this->reason;
-				
+
 				if (isset($this->expires))
 					$expires = time() + $this->expires;
 				else
 					$expires = 0; // Ban indefinitely
-				
-				if (isset($this->reject))
-					$reject = $this->reject;
-				else
-					$reject = true;
-				
+
 				if (isset($this->all_boards))
 					$all_boards = $this->all_boards;
 				else
 					$all_boards = false;
-				
+
 				$range = parse_mask($_SERVER['REMOTE_ADDR']);
-				
+
 				if (isset($this->ban_type))
 					$ban_type = $this->ban_type;
 				else
 					$ban_type = FULL_BAN;
-				
+
 				$query = prepare("INSERT INTO `bans` (`status`, `range_type`,`range_start`,`range_end`,`mod`,`set`,`expires`,`reason`,`board`,`ban_type`) VALUES (0, :range_type, INET6_ATON(:range_start), INET6_ATON(:range_end), :mod, :set, :expires, :reason, :board, :ban_type)");
 				$query->bindValue(':range_type', $range['range_type'], PDO::PARAM_INT);
 				$query->bindValue(':range_start', $range['range_start']);
@@ -95,45 +90,43 @@ class Filter {
 				$query->bindValue(':mod', -1);
 				$query->bindValue(':set', time(), PDO::PARAM_INT);
 				$query->bindValue(':ban_type', $ban_type);
-				
+
 				if ($expires)
 					$query->bindValue(':expires', $expires);
 				else
 					$query->bindValue(':expires', null, PDO::PARAM_NULL);
-				
+
 				if ($reason)
 					$query->bindValue(':reason', $reason);
 				else
 					$query->bindValue(':reason', null, PDO::PARAM_NULL);
-				
-				
+
+
 				if ($all_boards)
 					$query->bindValue(':board', null, PDO::PARAM_NULL);
 				else
 					$query->bindValue(':board', $board['uri']);
-				
+
 				$query->execute() or error(db_error($query));
-				
-				if ($reject) {
-					if (isset($this->message))
-						error($message);
-					
-					checkBan($board['uri']);
-					exit;
-				}
-				
+
+				if (isset($this->message))
+					error($message);
+
+				checkBan($board['uri']);
+				exit;
+
 				break;
 			default:
 				error('Unknown filter action: ' . $this->action);
 		}
 	}
-	
+
 	public function check(array $post) {
 		foreach ($this->condition as $condition => $value) {
 			if (!$this->match($post, $condition, $value))
 				return false;
 		}
-		
+
 		/* match */
 		return true;
 	}
@@ -141,14 +134,13 @@ class Filter {
 
 function do_filters(array $post) {
 	global $config;
-	
+
 	if (!isset($config['flood_filters']))
 		return;
-	
+
 	foreach ($config['flood_filters'] as $arr) {
 		$filter = new Filter($arr);
 		if ($filter->check($post))
 			$filter->action();
 	}
 }
-
