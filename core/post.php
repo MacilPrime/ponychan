019b21e9089b79208310cf0f22264a26a92f2227
creeme <glavinnyl@gmail.com>
2015-11-13 01:35:32 -0500

    Removed the magic quotes fix that has been deprecated for a long time.

--- core/post.php
+++ core/post.php
@@ -7,15 +7,6 @@
 require 'inc/functions.php';
 require 'inc/anti-bot.php';
 
-// Fix for magic quotes
-if (get_magic_quotes_gpc()) {
-	function strip_array($var) {
-		return is_array($var) ? array_map('strip_array', $var) : stripslashes($var);
-	}
-
-	$_GET = strip_array($_GET);
-	$_POST = strip_array($_POST);
-}
 
 header("Cache-Control: no-cache, must-revalidate");
 
