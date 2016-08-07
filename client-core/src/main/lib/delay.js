0abc1264e79230da6ab9299faa411633e9e4bcd3
Macil Tech <maciltech@gmail.com>
2016-08-06 21:07:23 -0700

    Update dependencies, start using Flow with client-core.

--- client-core/src/main/lib/delay.js
+++ client-core/src/main/lib/delay.js
@@ -1,6 +1,8 @@
+/* @flow */
+
 import RSVP from 'rsvp';
 
-export default function delay(time, value) {
+export default function delay<T>(time: number, value: T): Promise<T> {
   return new RSVP.Promise(function(resolve) {
     setTimeout(() => {
       resolve(value);
--- client-core/test/delay.js
+++ client-core/test/delay.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 import assert from 'assert';
 import delay from '../src/main/lib/delay';
 
